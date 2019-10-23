9519424dcd527e43b6728fc9e40387fbe0d067f5
Máté Kavalecz <work@mattwolf.it>
2019-10-23 03:21:21 +0200

    Delete _config.cpp

--- src/_config.cpp
+++
@@ -1,9 +0,0 @@
-// Use this file to define which example to compile
-#define IOT_EXAMPLE_BASIC
-//#define IOT_EXAMPLE_PUMPKIN
-
-// enable debug messages on serial
-//#define IOT_DEBUG
-
-#include "example_basic.cpp"
-#include "example_pumpkin.cpp"
